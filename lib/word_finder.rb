2791ef311a78477b9dd185dde99386561a76ee04
squeemishly <squeemishly@gmail.com>
2017-04-07 14:27:08 -0600

    working on word_finder... unsuccessfully

--- lib/word_finder.rb
+++ lib/word_finder.rb
@@ -1,6 +1,48 @@
+require 'pry'
+
 class WordFinder
   def words
     File.read('/usr/share/dict/words').split("\n")
   end
-  
+
+  def for_letters(letters)
+    possible_words = []
+    binding.pry
+    ###### Try permutation. Maybe a join? Do it for each of the letter sizes?
+    ###### FUUUUUUUUUUUCK!!!
+    
+    # words.each do |word|
+    #   if word.chars.all? { |letter| letters.include? letter }
+    #     possible_words << word
+    #   # binding.pry
+    #   end
+    # end
+    possible_words
+  end
 end
+
+# You can load up a huge list of words using the OS X internal dictionary
+# like so:
+#
+# ```ruby
+# File.read('/usr/share/dict/words').split("\n")
+# ```
+#
+# That instruction will return you a massive array of strings.
+#
+# Let's use that to cheat at Scrabble. Write a `WordFinder` with a `for_letters`
+# methods that finds all the words a player can play, using their letters.
+#
+# ```ruby
+# finder = WordFinder.new
+# finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o'])       # => ["a", "ae", "aer", "aero", "ape", "aper", "apert", "apex", ...
+# finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o']).count # => 141
+# ```
+#
+# Then build a second method named `for_letters_with` that takes a list of available letters along
+# with *one required letter*:
+#
+# ```ruby
+# finder.for_letters_with(['e', 'x', 'a', 'p', 't', 'o'], 'r')        # => ["aer", "aero", "aper", "apert", "aport", "ar", "are", "art", "arx", "ear", "er", "era", "export", "extra", ...
+# finder.for_letters_with(['e', 'x', 'a', 'p', 't', 'o'], 'r').count  # => 85
+# ```
--- test/word_finder_test.rb
+++ test/word_finder_test.rb
@@ -5,5 +5,18 @@ require 'minitest/pride'
 require 'pry'
 
 class WordFinderTest < Minitest::Test
+  def test_it_exists
+    word_finder = WordFinder.new
+    assert_instance_of WordFinder, word_finder
+  end
 
+  def test_it_can_access_the_words
+    assert_instance_of Array, WordFinder.new.words
+  end
+
+  def test_it_can_find_all_the_words_given_a_set_of_letters
+    finder = WordFinder.new
+    all_words = finder.for_letters(['r', 'e', 'x', 'a', 'p', 't', 'o'])
+    assert_equal 141, all_words.count
+  end
 end
